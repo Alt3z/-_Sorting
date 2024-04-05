cb2ad7359cd5421731709f28aae54ae2e96b0c8e
Фёдор Онищенко <166149674+Alt3z@users.noreply.github.com>
2024-04-06 01:06:34 +0500

    Add files via upload

---
+++ Библиотека - сортировки посредством выбора/!Библиотека посредством выбора.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include "LibZapoln.h"
+#include "LibVivod.h"
+#include "ViborSort.h"
+
+using namespace std;
+
+void main()
+{
+    setlocale(LC_ALL, "RUS");
+
+    int n;
+    cout << "Введите кол-во элементов: ";
+    cin >> n;
+
+    int* m = new int[n];
+
+    cout << endl << "Сортировка выбором" << endl;
+    cout << "Массив случайным способом заполнения: ";
+    Rand(m, n);
+    Vivod(m, n);
+    SortViborom(m, n);//сортировка выбором
+    cout << "Отсортированный массив: ";
+    Vivod(m, n);
+
+    cout << endl << "Квадратичная сортировка" << endl;
+    cout << "Массив случайным способом заполнения: ";
+    Rand(m, n);
+    Vivod(m, n);
+    KvadratSort(m, n);//Квадратичная сортировка
+
+    cout << endl <<endl << "Пирамидальная сортировка" << endl;
+    cout << "Массив случайным способом заполнения: ";
+    Rand(m, n);
+    Vivod(m, n);
+    PirSort(m, n);//Пирамидальная сортировка
+    cout << "Отсортированный массив: ";
+    Vivod(m, n);
+
+    system("pause");
+}
---
+++ Библиотека - сортировки посредством выбора/ViborSort.h
@@ -0,0 +1,5 @@
+void SortViborom(int* MAS, int n);
+void KvadratSort(int a[], int n);
+
+int PIR(int A[], int i, int N);
+void PirSort(int A[], int n);
